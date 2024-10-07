16acf516b34e0a7ddeaee640d68badb830eea8f1
[Pral Argade] <[pralargade@outlook.com]>
2024-10-07 10:47:14 -0400

    Add random math functionality to project

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,30 @@
 const topics = ['HTML', 'CSS', 'GIT', 'JavaScript'];
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
+function listTopics(){
+    
 for (let x = 0; x < topics.length; x++) {
     console.log(topics[x]);
    }
+}
 
-/*
+function selectTopic(){
 
-if (topic === 'HTML') {
+if (randomTopic === 'HTML') {
     console.log("Let's study HTML!");
-  } else if (topic === 'CSS') {
+  } else if (randomTopic === 'CSS') {
     console.log("Let's study CSS!");
-  } else if (topic === 'GIT') {
+  } else if (randomTopic === 'GIT') {
     console.log("Let's study GIT!");
-  } else if (topic === 'JavaScript') {
+  } else if (randomTopic === 'JavaScript') {
     console.log("Let's study JavaScript!");
   } else {
     console.log('Please try again!');
-  }
 
-*/
- 
+    
+  }
+}
+console.log('Here are the topics we learned through the PreWork');
+ listTopics()
+ console.log('Which topic should we study first?');
+ selectTopic()
